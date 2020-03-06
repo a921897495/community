4927aa996d5a2908a1379f093bb1cf96d80d8e2c
a921897495 <32760449+a921897495@users.noreply.github.com>
2020-03-06 09:19:19 +0800

    登录功能-保存cookie

--- src/main/java/com/sun/community/controller/AuthorizeController.java
+++ src/main/java/com/sun/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -26,8 +28,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code ,
-                           @RequestParam(name = "state")String state){
-
+                           @RequestParam(name = "state")String state,
+                            HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
 
         accessTokenDTO.setClient_id(clientID);
@@ -38,10 +40,15 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
 
-        System.out.println(githubUser.getName());
-        System.out.println(githubUser.getBio());
-        System.out.println(githubUser.getId());
+        if (githubUser!=null){
+            //登陆成功，写cookie和session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
 
-        return "index";
+        }else {
+            //登陆失败，重新登陆
+            return "redirect:/";
+        }
+
     }
 }
