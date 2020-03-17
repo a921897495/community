2b01c25e8728027988b290eee42346f44e5ab55a
a921897495 <32760449+a921897495@users.noreply.github.com>
2020-03-18 00:53:02 +0800

    拆分导航栏，完成部分个人中心

--- src/main/java/com/sun/community/mapper/QuestionMapper.java
+++ src/main/java/com/sun/community/mapper/QuestionMapper.java
@@ -21,4 +21,7 @@ public interface QuestionMapper {
 
     @Select("select count(1) from table_question")
     Integer count();
+
+    @Select("select * from table_question where creator=#{userId} limit #{offset},#{size}")
+    List<Question> listByUserId(@Param("userId") Integer userId, @Param(value = "offset") Integer offset, @Param(value = "size")Integer size);
 }
