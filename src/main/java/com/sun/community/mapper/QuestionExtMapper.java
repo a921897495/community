11fb8fe941f4fe3eeb38b6b8f1a1bacade5be1bc
a921897495 <32760449+a921897495@users.noreply.github.com>
2020-04-24 03:58:15 +0800

    完成相关问题的展示。标签 添加站长微信

--- src/main/java/com/sun/community/mapper/QuestionExtMapper.java
+++ src/main/java/com/sun/community/mapper/QuestionExtMapper.java
@@ -13,4 +13,9 @@ public interface QuestionExtMapper {
 
     int incView(Question record);
     int incCommentCount(Question record);
+    List<Question> selectRelated(Question question);
+
+   // Integer countBySearch(QuestionQueryDTO questionQueryDTO);
+
+    //List<Question> selectBySearch(QuestionQueryDTO questionQueryDTO);
 }
--- src/main/java/com/sun/community/service/QuestionService.java
+++ src/main/java/com/sun/community/service/QuestionService.java
@@ -10,13 +10,16 @@ import com.sun.community.mapper.UserMapper;
 import com.sun.community.model.Question;
 import com.sun.community.model.QuestionExample;
 import com.sun.community.model.User;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class QuestionService {
@@ -56,7 +59,9 @@ public class QuestionService {
         paginationDTO.setPagination(totalPage, page);
 
         Integer offset = page < 1 ? 0 : size * (page - 1);
-        List<Question> questions = questionMapper.selectByExampleWithRowbounds(new QuestionExample(), new RowBounds(offset, size));
+        QuestionExample questionExample = new QuestionExample();
+        questionExample.setOrderByClause("gmt_create desc");
+        List<Question> questions = questionMapper.selectByExampleWithRowbounds(questionExample, new RowBounds(offset, size));
         List<QuestionDTO> questionDTOList = new ArrayList<QuestionDTO>();
 
         for (Question question : questions) {
@@ -173,5 +178,29 @@ public class QuestionService {
         question.setId(id);
         question.setViewCount(1);
         questionExtMapper.incView(question);
+    }
+
+    public List<QuestionDTO> selectRelated(QuestionDTO queryDTO) {
+        if (StringUtils.isBlank(queryDTO.getTag())) {
+            return new ArrayList<>();
+        }
+        String[] tags = StringUtils.split(queryDTO.getTag(), ",");
+        String regexpTag = Arrays
+                .stream(tags)
+                .filter(StringUtils::isNotBlank)
+                .map(t -> t.replace("+", "").replace("*", "").replace("?", ""))
+                .filter(StringUtils::isNotBlank)
+                .collect(Collectors.joining("|"));
+        Question question = new Question();
+        question.setId(queryDTO.getId());
+        question.setTag(regexpTag);
+
+        List<Question> questions = questionExtMapper.selectRelated(question);
+        List<QuestionDTO> questionDTOS = questions.stream().map(q -> {
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(q, questionDTO);
+            return questionDTO;
+        }).collect(Collectors.toList());
+        return questionDTOS;
     }
 }
